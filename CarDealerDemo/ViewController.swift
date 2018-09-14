7c2a37cf0403d2048e0832e17897a0b8c5cca93b
Truong Vo <truong@buuuk.com>
2018-09-14 17:08:44 +0700

    read json from file

--- CarDealerDemo/ViewController.swift
+++ CarDealerDemo/ViewController.swift
@@ -13,6 +13,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if let path = Bundle.main.path(forResource: "models-json", ofType: "txt") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+                dump(jsonResult)
+            }
+            catch {
+                // handle error
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
